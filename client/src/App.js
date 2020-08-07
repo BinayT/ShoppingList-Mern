6941e38a8b908d7ab782a55cd3c79d3dfc00117c
Binay <binayprogramming@gmail.com>
2020-08-07 17:26:34 +0200

    Client setup & ReactStrap implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Appnavbar from "./components/Appnavbar";
 
 export default () => (
   <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
+    <Appnavbar />
+    <h1>Helloo</h1>
   </>
 );
